6aaecb9f320337d39ccbd2828416d0ca7880809e
ratnov2 <anton.ratnov@yandex.ru>
2023-07-22 13:02:41 +0300

    add mobile

--- test_2/src/index.tsx
+++ test_2/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/style/global.scss";
 import App from "./App";
+import Layout from "./assets/Layout/Layout";
 
 const root = ReactDOM.createRoot(
  document.getElementById("root") as HTMLElement
 );
 root.render(
  <React.StrictMode>
-  <App />
+  <Layout>
+   <App />
+  </Layout>
  </React.StrictMode>
 );
 
